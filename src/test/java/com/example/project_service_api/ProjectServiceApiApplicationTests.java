620bd1c1d3a3f3e97a01c97ad4d62b3a623b9c8c
denisa.ciuciuc <denisa.ciuciuc@stud.etti.upb.ro>
2025-03-03 21:14:03 +0200

    chore: initial commit

---
+++ src/test/java/com/example/project_service_api/ProjectServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
